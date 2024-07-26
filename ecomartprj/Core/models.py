f5c93900258864baeabdac95daf79663b7532079
mboufern <boufernana17@gmail.com>
2024-07-26 23:33:31 +0000

    Created models

--- ecomartprj/Core/models.py
+++ ecomartprj/Core/models.py
@@ -1,3 +1,76 @@
 from django.db import models
 
-# Create your models here.
+class Categorie(models.Model):
+    nom = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+class Couleur(models.Model):
+    nom = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+class Qualite(models.Model):
+    nom = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+    
+class Tableau(models.Model):
+    class Etat(models.TextChoices):
+        EN_VENTE = 'EN_VENTE', 'En vente'
+        EN_ENCHERE = 'EN_ENCHERE', 'En enchere'
+        VENDU = 'VENDU', 'Vendu'
+        RESERVE = 'RESERVE', 'Réservé'
+
+    titre = models.CharField(max_length=128, unique=True)
+    prix = models.FloatField(blank=True, null=True)
+    date = models.DateTimeField()
+    description = models.TextField()
+    etat = models.CharField(max_length=12, choices=Etat.choices, default=Etat.RESERVE,)
+    categories = models.ManyToManyField(Categorie)
+    couleurs = models.ManyToManyField(Couleur)
+    qualites = models.ForeignKey(Qualite, on_delete=models.SET_NULL, null=True,)
+
+    def __str__(self):
+        return self.titre
+    
+class Canvas(models.Model):
+    emplacement = models.FloatField(blank=True, null=True)
+    largeur = models.SmallIntegerField()
+    hauteur = models.SmallIntegerField()
+    tableau = models.ForeignKey(Tableau, on_delete=models.CASCADE, related_name='canvases')
+
+    def __str__(self):
+        return f"L{self.largeur} x H{self.hauteur}"
+
+class Commande(models.Model):
+    nomcomplet = models.CharField(max_length=128,)
+    tel = models.CharField(max_length=128,)
+    livraison = models.BooleanField(blank=True, null=True,)
+    adresse = models.CharField(max_length=128, blank=True, null=True,)
+    date = models.DateField()
+    message = models.TextField(blank=True, null=True,)
+    tableau = models.ForeignKey(Tableau, on_delete=models.SET_NULL, null=True, related_name='commandes')
+
+class Enchere(models.Model):
+    dateDebut = models.DateField()
+    dateFin = models.DateField()
+    prixDebut = models.FloatField()
+    prixFin = models.FloatField(blank=True, null=True,)
+    tableau = models.ForeignKey(Tableau, on_delete=models.SET_NULL, null=True, related_name='echeres')
+
+class Declaration(models.Model):
+    nomcomplet = models.CharField(max_length=128, )
+    tel = models.CharField(max_length=128, )
+    prix = models.FloatField()
+    date = models.DateField()
+    enchere = models.ForeignKey(Enchere, on_delete=models.CASCADE, related_name='declarations')
+
+class Recu(models.Model):
+    prixAchat = models.FloatField()
+    dateAchat = models.DateField()
+    commande = models.ForeignKey(Commande, on_delete=models.SET_NULL, blank=True, null=True, related_name='recuscom')
+    enchere = models.ForeignKey(Enchere, on_delete=models.SET_NULL, blank=True, null=True, related_name='recusdec')
